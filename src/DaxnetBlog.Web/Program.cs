67b7f814fd20ab874444e6eb9c67fa8e6b4a5f49
daxnet <daxnet@outlook.com>
2016-11-28 21:03:51 +0800

    Added the logging of the date on which the user performed the last successful logon. Added the HTTPS support.

--- src/DaxnetBlog.Web/Program.cs
+++ src/DaxnetBlog.Web/Program.cs
@@ -4,16 +4,33 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using System.Security.Cryptography.X509Certificates;
+using DaxnetBlog.Common;
 
 namespace DaxnetBlog.Web
 {
     public class Program
     {
+        const string SslCertificateFileName = @"daxnetme.pfx";
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls("http://+:5000/")
+            var sslCertificatePassword = EnvironmentVariables.SslCertificatePassword;
+            IWebHostBuilder builder = new WebHostBuilder();
+
+            if (string.IsNullOrEmpty(sslCertificatePassword))
+            {
+                builder = builder.UseKestrel().UseUrls("http://+:5000/");
+            }
+            else
+            {
+                builder = builder.UseKestrel(options =>
+                    options.UseHttps(new X509Certificate2(SslCertificateFileName, sslCertificatePassword)))
+                .UseUrls("http://+:5000/", "https://+:5001/");
+            }
+            
+            var host = builder
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
